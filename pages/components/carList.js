d2dadfba92a18c9ba9f3021e44d91b6469fcd125
DwsCode88 <42653240+DwsCode88@users.noreply.github.com>
2022-02-21 21:39:01 -0500

    Add files via upload

---
+++ pages/components/carList.js
@@ -0,0 +1,27 @@
+export const carList = [
+  {
+    imgUrl: "https://i.ibb.co/cyvcpfF/uberx.png",
+    service: "CARX",
+    multiplier: 1,
+  },
+  {
+    imgUrl: "https://i.ibb.co/YDYMKny/uberxl.png",
+    service: "CARXL",
+    multiplier: 1.5,
+  },
+  {
+    imgUrl: "https://i.ibb.co/Xx4G91m/uberblack.png",
+    service: "CARBlack",
+    multiplier: 2,
+  },
+  {
+    imgUrl: "https://i.ibb.co/cyvcpfF/uberx.png",
+    service: "CARComfort",
+    multiplier: 1.2,
+  },
+  {
+    imgUrl: " https://i.ibb.co/1nStPWT/uberblacksuv.png",
+    service: "CARBlack SUV",
+    multiplier: 2.8,
+  },
+];
